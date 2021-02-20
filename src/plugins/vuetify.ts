56ff8fde72372c05230b4bd430915cefb06a0b5c
wenfei <thexwant@gmail.com>
2021-02-20 18:54:44 +0800

    feat: vue周边以及vuetify周边依赖外置

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,11 +1,9 @@
 import Vue from 'vue'
-import Vuetify, { VSnackbar, VBtn } from 'vuetify/lib'
+import Vuetify from 'vuetify'
+import "vuetify/dist/vuetify.min.css"
 import VuetifyMessageSnackbar from 'vuetify-message-snackbar'
 
 Vue.use(Vuetify)
-Vue.component('v-snackbar', VSnackbar)
-Vue.component('v-btn', VBtn)
-
 Vue.use(VuetifyMessageSnackbar)
 
 export default new Vuetify()
